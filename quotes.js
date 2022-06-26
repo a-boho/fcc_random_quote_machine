e7e02ef9ef4badcd92f02bf7643de4e6be531c12
a-boho <96634982+a-boho@users.noreply.github.com>
2022-06-26 16:38:42 -0400

    Create quotes.js

---
+++ quotes.js
@@ -0,0 +1,106 @@
+const QUOTEBANK = [
+    {
+        quote: "If you don't believe it or don't get it, I don't have the time to try to convince you, sorry.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "It might make sense just to get some in case it catches on.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "The root problem with conventional currency is all the trust that's required to make it work. The central bank must be trusted not to debase the currency, but the history of fiat currencies is full of breaches of that trust.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "I'm sure that in 20 years there will either be very large transaction volume or no volume.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "I've been working on a new electronic cash system that's fully peer-to-peer, with no trusted third party.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "The nature of Bitcoin is such that once version 0.1 was released, the core design was set in stone for the rest of its lifetime.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Being open source means anyone can independently review the code. If it was closed source, nobody could verify the security. I think it's essential for a program of this nature to be open source.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Lost coins only make everyone else's coins worth slightly more. Think of it as a donation to everyone.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "It is a global distributed database, with additions to the database by consent of the majority.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "I've developed a new open source P2P e-cash system called Bitcoin. It's completely decentralized, with no central server or trusted parties, because everything is based on crypto proof instead of trust.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Banks must be trusted to hold our money and transfer it electronically, but they lend it out in waves of credit bubbles with barely a fraction in reserve. We have to trust them with our privacy, trust them not to let identity thieves drain our accounts. Their massive overhead costs make micropayments impossible.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "It's very attractive to the libertarian viewpoint if we can explain it properly. I'm better with code than with words though.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "The heat from your computer is not wasted if you need to heat your home.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Governments are good at cutting off the heads of a centrally controlled networks like Napster, but pure P2P networks like Gnutella and Tor seem to be holding their own.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Bitcoin would be convenient for people who don't have a credit card or don't want to use the cards they have.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "The result is a distributed system with no single point of failure. Users hold the crypto keys to their own money and transact directly with each other, with the help of the P2P network to check for double-spending.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "For greater privacy, it's best to use bitcoin addresses only once.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "A purely peer-to-peer version of electronic cash would allow online payments to be sent directly from one party to another without going through a financial institution.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Imagine if gold turned to lead when stolen.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "I've been working on bitcoin's design since 2007. At some point I became convinced there was a way to do this without any trust required at all and couldn't resist to keep thinking about it.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "Bitcoin generation should end up where it's cheapest. Maybe that will be in cold climates where there's electric heat, where it would be essentially free.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "At first, most users would run network nodes, but as the network grows beyond a certain point, it would be left more and more to specialists with server farms of specialized hardware. A server farm would only need to have one node on the network and the rest of the LAN connects with that one node.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "When there are multiple double-spent versions of the same transaction, one and only one will become valid.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "In a few decades when the reward gets too small, the transaction fee will become the main compensation for nodes.",
+        author: "Satoshi Nakamoto"
+    },
+    {
+        quote: "I anticipate there will never be more than 100K nodes, probably less. It will reach an equilibrium where it's not worth it for more nodes to join in. The rest will be lightweight clients, which could be millions.",
+        author: "Satoshi Nakamoto"
+    }
+];
